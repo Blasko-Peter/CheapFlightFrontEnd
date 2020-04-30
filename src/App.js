dd4365cb606c47afb47ad1091153e0f3a8e65329
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-04-30 21:38:34 +0200

    create new component - search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import "./styles/app.css";
 import Navbar from "./components/Navbar";
+import Search from './components/Search';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Search />
       </div>
     );
   }
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/search.css';
+
+function Search() {
+    return (
+        <div id="search-container">
+            <h2>Search bar</h2>           
+        </div>
+    )
+}
+
+export default Search;
