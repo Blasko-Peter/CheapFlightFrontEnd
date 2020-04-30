35a8724de5375405550b9ac6d036e53e455b71b1
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-04-30 22:22:01 +0200

    add some functions to Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,49 @@
 import React from 'react';
 import '../styles/search.css';
+import { MDBBtn } from "mdbreact";
 
 function Search() {
     return (
         <div id="search-container">
-            <h2>Search bar</h2>           
+            <div id="cities">
+                <div className="city">
+                    <select className="browser-default custom-select">
+                    <option>Choose your start city</option>
+                    <option value="1">Option 1</option>
+                    <option value="2">Option 2</option>
+                    <option value="3">Option 3</option>
+                    </select>
+                </div>
+                <div className="city">
+                    <select className="browser-default custom-select">
+                    <option>Choose your target city</option>
+                    <option value="1">Option 1</option>
+                    <option value="2">Option 2</option>
+                    <option value="3">Option 3</option>
+                    </select>
+                </div>
+            </div>
+            <div id="dates">
+                <div className="city">
+                    <select className="browser-default custom-select">
+                    <option>Choose your start city</option>
+                    <option value="1">Option 1</option>
+                    <option value="2">Option 2</option>
+                    <option value="3">Option 3</option>
+                    </select>
+                </div>
+                <div className="city">
+                    <select className="browser-default custom-select">
+                    <option>Choose your target city</option>
+                    <option value="1">Option 1</option>
+                    <option value="2">Option 2</option>
+                    <option value="3">Option 3</option>
+                    </select>
+                </div>
+            </div>
+            <div id="search-button">
+                <MDBBtn color="warning">Search</MDBBtn>
+            </div>
         </div>
     )
 }
