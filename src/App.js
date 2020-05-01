d0965d96be1134ed30a43d96d3a39416cd64a7d9
Blasko-Peter <blasko.peter.hu@gmail.com>
2020-05-01 22:07:07 +0200

    can get cities from backend

--- src/App.js
+++ src/App.js
@@ -4,11 +4,36 @@ import Navbar from "./components/Navbar";
 import Search from './components/Search';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      loading: false,
+      cities: []
+    }
+  }
+
+  componentDidMount(){
+    this.uploadCities("http://localhost:8000/getAllCities")
+  }
+
+  uploadCities = (url) => {
+    this.setState({loading: true})
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+            loading: false,
+            cities: data
+          })
+        })
+  }
+
   render() {
+    const Searching = <Search cities={this.state.cities} />
     return (
       <div>
         <Navbar />
-        <Search />
+        {Searching}
       </div>
     );
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,24 +2,21 @@ import React from 'react';
 import '../styles/search.css';
 import { MDBBtn } from "mdbreact";
 
-function Search() {
+function Search(props) {
+    const allcities = props.cities.map(city => <option key={city} value={city}>{city}</option>);
     return (
         <div id="search-container">
             <div id="cities">
                 <div className="city">
                     <select className="browser-default custom-select">
                     <option>Choose your start city</option>
-                    <option value="1">Option 1</option>
-                    <option value="2">Option 2</option>
-                    <option value="3">Option 3</option>
+                    {allcities}
                     </select>
                 </div>
                 <div className="city">
                     <select className="browser-default custom-select">
                     <option>Choose your target city</option>
-                    <option value="1">Option 1</option>
-                    <option value="2">Option 2</option>
-                    <option value="3">Option 3</option>
+                    {allcities}
                     </select>
                 </div>
             </div>
